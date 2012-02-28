b9d13aff6583aab06ff17117b5f522618eeb054d
David Bellingroth <dbellingroth@gmail.com>
2012-02-28 15:53:20 +0100

    DBPediaImporter speichert jetzt auch die Geokoordinaten der Orte...

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-
+  
   has_many :residences
   has_many :people, :through => :residences
 
--- lib/dbpediaimporter.rb
+++ lib/dbpediaimporter.rb
@@ -8,13 +8,15 @@ class DbPediaImporter
     opts.each do |k,v|
     	filter += "FILTER(?#{k} = '#{v}'@en). "
     end
-    query = "SELECT DISTINCT ?person ?firstname ?lastname ?birthdate ?birthplace ?birthplacename ?thumbnail WHERE {
+    query = "SELECT DISTINCT ?person ?firstname ?lastname ?birthdate ?birthplace ?birthplacename ?thumbnail ?bplat ?bplon WHERE {
     ?person rdf:type foaf:Person .
     ?person foaf:givenName ?firstname .
     ?person foaf:surname ?lastname .
     ?person dbpedia-owl:birthDate ?birthdate .
     ?person dbpedia-owl:birthPlace ?birthplace .
     ?birthplace rdfs:label ?birthplacename .
+    ?birthplace geo:lat ?bplat.
+    ?birthplace geo:long ?bplon.
     ?person dbpedia-owl:thumbnail ?thumbnail .
 		#{filter}
     }"
@@ -28,13 +30,17 @@ class DbPediaImporter
       birthplace_url = solution.to_hash[:birthplace].to_s
       birthplacename = solution.to_hash[:birthplacename].to_s
       thumbnail = solution.to_hash[:thumbnail].to_s
- 
+      lat = solution.to_hash[:bplat].to_s.to_f
+      lon = solution.to_hash[:bplon].to_s.to_f
+
  			person = Person.find_or_initialize_by_url(person_url)
       person.update_attributes(:url => person_url, :firstname => firstname, :lastname => lastname, :thumbnail => thumbnail, :birthdate => birthdate)
       
       place=Location.find_or_initialize_by_url(birthplace_url)
       place.url = birthplace_url
       place.name = birthplacename
+      place.lat = lat
+      place.lon = lon
       place.save
       person.residences.create(:location_id => place.id, :status => "birthplace")
       p person.inspect 
