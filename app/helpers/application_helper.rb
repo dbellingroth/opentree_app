1e50c8d7e14b5660c5767015782c8312bb1b4fd6
David Bellingroth <dbellingroth@gmail.com>
2012-03-05 18:07:39 +0100

    Löschen Knopf funktioniert jetzt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def display_person_count
+    "There are #{Person.count} people in the app"
+  end
 end
--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,5 +1,4 @@
 class Relation < ActiveRecord::Base
   belongs_to :person, :class_name => "Person", :foreign_key => "base_person_id"
   belongs_to :related_person, :class_name => "Person", :foreign_key => "related_person_id"
-  
 end
