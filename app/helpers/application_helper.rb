e43d9f0e0733a81ceab9d70c610e28097c63d3e7
doro <doro@doro-F5SR.(none)>
2012-03-06 12:24:40 +0100

    Verwandte zählen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def display_person_count
     "There are #{Person.count} people in the app"
   end
+  
+  def persons_relations_count(person)
+    person.relatives.count
+  end
 end
